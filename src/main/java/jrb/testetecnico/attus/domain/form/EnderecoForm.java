55fce05703ec991ed1b92e44b1c6ba9fa69cd831
jardessonrb <jardessonrs1117@gmail.com>
2024-05-05 15:19:39 -0300

    feat: Criado classe de entrada EnderecoForm

--- src/main/java/jrb/testetecnico/attus/domain/form/EnderecoForm.java
+++ src/main/java/jrb/testetecnico/attus/domain/form/EnderecoForm.java
@@ -1,2 +1,28 @@
-package jrb.testetecnico.attus.domain.form;public record EnderecoForm() {
+package jrb.testetecnico.attus.domain.form;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record EnderecoForm(
+        @NotNull
+        @NotBlank
+        String logradouro,
+
+        @NotNull
+        @NotBlank
+        String cep,
+
+        @NotNull
+        @NotBlank
+        String cidade,
+
+        @NotNull
+        @NotBlank
+        String estado,
+
+        @Min(0)
+        Integer numero
+
+) {
 }
