591daa9982e4749570bc5e2eb6ca9be978c3244c
jardessonrb <jardessonrs1117@gmail.com>
2024-05-06 19:50:19 -0300

    feat: Criada a interface para EnderecoService

--- src/main/java/jrb/testetecnico/attus/service/endereco/EnderecoService.java
+++ src/main/java/jrb/testetecnico/attus/service/endereco/EnderecoService.java
@@ -1,2 +1,21 @@
-package jrb.testetecnico.attus.service.endereco;public interface EnderecoService {
+package jrb.testetecnico.attus.service.endereco;
+
+import jrb.testetecnico.attus.domain.dto.EnderecoDto;
+import jrb.testetecnico.attus.domain.form.EnderecoForm;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface EnderecoService {
+
+    EnderecoDto inserir(UUID pessoaId, EnderecoForm enderecoForm);
+
+    List<EnderecoDto> buscarEnderecos(UUID pessoaId);
+
+    Void definirEnderecoPrincipal(UUID pessoaId, UUID enderecoId);
+
+    EnderecoDto atualizar(UUID enderecoId, EnderecoForm enderecoForm);
+
+    EnderecoDto buscarEnderecosPorId(UUID enderecoId);
+
 }
