6240556f9ea3438330cc971140e5d8c8582fe683
jardessonrb <jardessonrs1117@gmail.com>
2024-05-06 19:45:52 -0300

    feat: Criado Repository para MunicipioEstadoModel

--- src/main/java/jrb/testetecnico/attus/domain/repository/MunicipioEstadoRepository.java
+++ src/main/java/jrb/testetecnico/attus/domain/repository/MunicipioEstadoRepository.java
@@ -3,5 +3,5 @@ package jrb.testetecnico.attus.domain.repository;
 import jrb.testetecnico.attus.domain.model.MunicipioEstadoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MunicipioEstado extends JpaRepository<MunicipioEstadoModel, Long> {
+public interface MunicipioEstadoRepository extends JpaRepository<MunicipioEstadoModel, Long> {
 }
