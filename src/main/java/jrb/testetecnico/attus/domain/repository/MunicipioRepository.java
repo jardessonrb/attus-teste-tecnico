a0aa4e2a7a582058d5c7903cb4b7829b2a239505
jardessonrb <jardessonrs1117@gmail.com>
2024-05-07 22:39:56 -0300

    rrefactor: Refatoração no nome do Repository para MunicipioRepository

--- src/main/java/jrb/testetecnico/attus/domain/repository/MunicipioRepository.java
+++ src/main/java/jrb/testetecnico/attus/domain/repository/MunicipioRepository.java
@@ -6,12 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface MunicipioEstadoRepository extends JpaRepository<MunicipioEstadoModel, Long> {
+@Repository
+public interface MunicipioRepository extends JpaRepository<MunicipioEstadoModel, Long> {
 
     @Query("SELECT me FROM MunicipioEstadoModel me WHERE ((:filtro is null or  me.nomeMunicipio ILIKE CONCAT('%', :filtro, '%')) and (:ufEstado IS NULL OR me.ufEstado = UPPER(:ufEstado)))")
     Page<MunicipioEstadoModel> buscarMunicipios(@Param("filtro") String filtro, @Param("ufEstado") String ufEstado, Pageable paginacao);
-
-    @Query("SELECT me FROM MunicipioEstadoModel me WHERE (:filtro is null or  me.nomeEstado ILIKE CONCAT('%', :filtro, '%') or me.ufEstado = UPPER(:filtro))")
-    Page<MunicipioEstadoModel> buscarEstados(@Param("filtro") String filtro, Pageable paginacao);
 }
